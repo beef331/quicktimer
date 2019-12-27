8e245e95a01b845205b6ab06c8179aeb78b34ef6
Lorenzo Fontoura <rellfysilveira@gmail.com>
2019-12-27 20:09:29 +1000

    Add async support

--- Runtime/TimerExample.cs
+++ Runtime/TimerExample.cs
@@ -7,18 +7,27 @@ public class TimerExample : MonoBehaviour
 {
 	private bool canSpam = true;
 
-	void Update()
+	async void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.A) && canSpam)
 		{
+            // Timer using callbacks
 			canSpam = false;
 			new Timer( .3f, ResetSpam);
 		}
+
+        if (Input.GetKeyDown(KeyCode.S) && canSpam)
+		{
+            // Timer using async/await
+			canSpam = false;
+            await Timer.Start(.3f);
+            ResetSpam();
+        }
 	}
 
 	void ResetSpam()
 	{
 		canSpam = true;
-		Debug.Log("Can tap A again.");
+		Debug.Log("Can tap A/S again.");
 	}
 }
